c4c3cf7d27ef54d8bfe4d1d2c5a06bbd6216256a
sumoo13 <jean_philipperoux@hotmail.com>
2020-01-23 14:36:22 +0100

    add two fonction : get_cockpit & get_hvac

--- Test/MyRenault.py
+++ Test/MyRenault.py
@@ -8,7 +8,7 @@ import urllib
 
 async def get_android_config(session, location):
     url = 'https://renault-wrd-prod-1-euw1-myrapp-one.s3-eu-west-1.amazonaws.com/configuration/android/config_' + location + '.json'
-    print(url)
+    #print(url)
     async with session.get(url) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -22,7 +22,7 @@ async def get_android_config(session, location):
 async def get_gigyasession(session, gigyarooturl, gigyaapikey, loginID, password):
     payload = {'loginID': loginID, 'password': password, 'apiKey': gigyaapikey}
     url = gigyarooturl + '/accounts.login?' + urllib.parse.urlencode(payload)
-    print(url)
+    #print(url)
     async with session.get(url) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -36,7 +36,7 @@ async def get_gigyasession(session, gigyarooturl, gigyaapikey, loginID, password
 async def get_gigyaaccount(session, gigyarooturl, gigyaapikey, gigyacookievalue):
     payload = {'oauth_token': gigyacookievalue}
     url = gigyarooturl + '/accounts.getAccountInfo?' + urllib.parse.urlencode(payload)
-    print(url)
+    #print(url)
     async with session.get(url) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -50,7 +50,7 @@ async def get_gigyaaccount(session, gigyarooturl, gigyaapikey, gigyacookievalue)
 async def get_gigyajwt(session, gigyarooturl, gigyaapikey, gigyacookievalue):
     payload = {'oauth_token': gigyacookievalue, 'fields': 'data.personId,data.gigyaDataCenter', 'expiration': 900}
     url = gigyarooturl + '/accounts.getJWT?' + urllib.parse.urlencode(payload)
-    print(url)
+    #print(url)
     async with session.get(url) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -80,7 +80,7 @@ async def get_kamereontoken(session, kamereonrooturl, kamereonapikey, gigya_jwtt
     payload = {'country': 'FR'}
     headers = {'x-gigya-id_token': gigya_jwttoken, 'apikey': kamereonapikey}
     url = kamereonrooturl + '/commerce/v1/accounts/' + accountId + '/kamereon/token?' + urllib.parse.urlencode(payload)
-    print(url)
+    #print(url)
     async with session.get(url, headers=headers) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -94,7 +94,35 @@ async def get_kamereontoken(session, kamereonrooturl, kamereonapikey, gigya_jwtt
 async def get_batterystatus(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, vin):
     headers = {'x-gigya-id_token': gigya_jwttoken, 'apikey': kamereonapikey, 'x-kamereon-authorization' : 'Bearer ' + kamereonaccesstoken}
     url = kamereonrooturl + '/commerce/v1/accounts/kmr/remote-services/car-adapter/v1/cars/' + vin + '/battery-status'
-    print(url)
+    #print(url)
+    async with session.get(url, headers=headers) as response:
+        responsetext = await response.text()
+        if responsetext == '':
+            responsetext = '{}'
+        jsonresponse = json.loads(responsetext)
+        if 'message' in jsonresponse:
+            self.tokenData = None
+            raise MyRenaultServiceException(jsonresponse['message'])
+        return jsonresponse
+
+async def get_cockpit(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, vin):
+    headers = {'x-gigya-id_token': gigya_jwttoken, 'apikey': kamereonapikey, 'x-kamereon-authorization' : 'Bearer ' + kamereonaccesstoken}
+    url = kamereonrooturl + '/commerce/v1/accounts/kmr/remote-services/car-adapter/v1/cars/' + vin + '/cockpit'
+    #print(url)
+    async with session.get(url, headers=headers) as response:
+        responsetext = await response.text()
+        if responsetext == '':
+            responsetext = '{}'
+        jsonresponse = json.loads(responsetext)
+        if 'message' in jsonresponse:
+            self.tokenData = None
+            raise MyRenaultServiceException(jsonresponse['message'])
+        return jsonresponse
+
+async def get_hvac(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, vin):
+    headers = {'x-gigya-id_token': gigya_jwttoken, 'apikey': kamereonapikey, 'x-kamereon-authorization' : 'Bearer ' + kamereonaccesstoken}
+    url = kamereonrooturl + '/commerce/v1/accounts/kmr/remote-services/car-adapter/v1/cars/' + vin + '/hvac-status'
+    #print(url)
     async with session.get(url, headers=headers) as response:
         responsetext = await response.text()
         if responsetext == '':
@@ -112,7 +140,7 @@ async def main():
 
 async def mainwithsession(session):
     # Load credentials.
-    in_file = open('credentials.json', 'r')
+    in_file = open('/srv/zoe/model/credentials.json', 'r')
     credentials = json.load(in_file)
     in_file.close()
 
@@ -122,7 +150,7 @@ async def mainwithsession(session):
     #in_file = open('android_config.json', 'r')
     #android_config = json.load(in_file)
     #in_file.close()
-    print('android_config')
+    #print('android_config')
     
     gigyarooturl = android_config['servers']['gigyaProd']['target']
     gigyaapikey = android_config['servers']['gigyaProd']['apikey']
@@ -136,7 +164,7 @@ async def mainwithsession(session):
     #in_file = open('gigya_session.json', 'r')
     #gigya_session = json.load(in_file)
     #in_file.close()
-    print('gigya_session')
+    #print('gigya_session')
     
     gigyacookievalue = gigya_session['sessionInfo']['cookieValue']
 
@@ -146,7 +174,7 @@ async def mainwithsession(session):
     #in_file = open('gigya_account.json', 'r')
     #gigya_account = json.load(in_file)
     #in_file.close()
-    print('gigya_account')
+    #print('gigya_account')
 
     gigya_jwt = await get_gigyajwt(session, gigyarooturl, gigyaapikey, gigyacookievalue)
     with open('gigya_jwt.json', 'w') as outfile:
@@ -154,7 +182,7 @@ async def mainwithsession(session):
     #in_file = open('gigya_jwt.json', 'r')
     #gigya_jwt = json.load(in_file)
     #in_file.close()
-    print('gigya_jwt')
+    #print('gigya_jwt')
     
     gigya_jwttoken= gigya_jwt['id_token']
     
@@ -166,19 +194,32 @@ async def mainwithsession(session):
     #in_file = open('kamereon_person.json', 'r')
     #kamereon_person = json.load(in_file)
     #in_file.close()
-    print('kamereon_person')
+    #print('kamereon_person')
 
     kamereonaccountid = kamereon_person['accounts'][0]['accountId']
 
     kamereon_token = await get_kamereontoken(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccountid)
     with open('kamereon_token.json', 'w') as outfile:
         json.dump(kamereon_token, outfile)
-    print('kamereon_token')
+    #print('kamereon_token')
     
     kamereonaccesstoken = kamereon_token['accessToken']
 
     kamereon_battery = await get_batterystatus(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, credentials['VIN'])
+    with open('kamereon_battery.json', 'w') as outfile:
+        json.dump(kamereon_battery, outfile)
     print(kamereon_battery)
+
+    kamereon_cockpit = await get_cockpit(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, credentials['VIN'])
+    with open('kamereon_cockpit.json', 'w') as outfile:
+        json.dump(kamereon_cockpit, outfile)
+    print(kamereon_cockpit)
+
+
+    kamereon_hvac = await get_hvac(session, kamereonrooturl, kamereonapikey, gigya_jwttoken, kamereonaccesstoken, credentials['VIN'])
+    with open('kamereon_hvac.json', 'w') as outfile:
+        json.dump(kamereon_hvac, outfile)
+    print(kamereon_hvac)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
